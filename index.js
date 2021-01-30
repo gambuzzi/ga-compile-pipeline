377b6abd85dbcc709cf7b199005088573f1b753a
Roberto Gambuzzi <roberto.gambuzzi@fabfitfun.com>
2021-01-30 16:59:56 +0000

    add some input paramters

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { exec } = require("child_process");
 const fs = require('fs');
+const { pid, env } = require('process');
+const { pipeline } = require('stream');
 const YAML = require('yaml');
 
 
 try {
-  fs.copyFile('../.github/actions/gambuzzi/ga-compile-pipeline/pipeline/pipeline.yaml', '.github/workflows/pipeline.yaml', (err) => {
-    if (err) throw err;
-    console.log('../.github/actions/gambuzzi/ga-compile-pipeline/pipeline/pipeline.yaml was copied to .github/workflows/pipeline.yaml');
-  });
+  const file = fs.readFileSync('../.github/actions/gambuzzi/ga-compile-pipeline/pipeline/pipeline.yaml', 'utf8');
+  const pipeline = YAML.parse(file);
+  
+  const program = core.getInput('program');
+  const registry = core.getInput('registry');
+  const img_name = core.getInput('img_name');
+  const maintainer = core.getInput('maintainer');
+  const owner = core.getInput('owner');
 
-  const commands = core.getInput('commands');
-  console.log(`commands ${commands}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  pipeline.env.PROGRAM = program;
+  pipeline.env.REGISTRY = registry;
+  pipeline.env.IMG_NAME = img_name;
+  pipeline.env.MAINTAINER = maintainer;
+  pipeline.env.OWNER = owner;
+
+  fs.writeFileSync('.github/workflows/pipeline.yaml', YAML.stringify(pipeline));
+  // fs.copyFile('../.github/actions/gambuzzi/ga-compile-pipeline/pipeline/pipeline.yaml', '.github/workflows/pipeline.yaml', (err) => {
+  //   if (err) throw err;
+  //   console.log('../.github/actions/gambuzzi/ga-compile-pipeline/pipeline/pipeline.yaml was copied to .github/workflows/pipeline.yaml');
+  // });
+
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
